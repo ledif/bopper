be496fc0b0dafad162a4c4cee2eb8ddb905cfd1a
Adam Fidel <refuse@gmail.com>
2014-07-19 00:54:08 -0500

    Cleanup and moving bop endpoint to root.

--- web.js
+++ web.js
@@ -1,36 +1,34 @@
-// web.js
-var express = require("express");
-var morgan  = require('morgan');
+var express = require("express"),
+    morgan  = require('morgan'),
+    request = require('request'),
+    cheerio = require('cheerio');
+
 var app = express();
 
-var request = require('request'),
-	cheerio = require('cheerio');
-
+// log that shiz
 app.use(morgan('dev'));
-//app.use(express.json());       // to support JSON-encoded bodies
-//app.use(express.urlencoded()); // to support URL-encoded bodies
 
-app.get('/bop', function(req, res) {
-	var uri = req.query.uri
+app.get('/', function(req, res) {
+  var uri = req.query.uri
 
-	request({uri : uri}, function(error, response, body){
-		if(!error && response.statusCode === 200)
-		{
-			var parsedDocument = cheerio.load(body);
-			var result = {
-				title: parsedDocument("meta[name='twitter:text:main_song_title']").attr("content"),
-				artist: parsedDocument("meta[name='twitter:text:main_song_artist']").attr("content"),
-				image: parsedDocument("meta[name='twitter:image:song_thumbnail_img']").attr("content")
-			}
+  request({uri : uri}, function(error, response, body) {
+    if (!error && response.statusCode === 200)
+    {
+      var parsedDocument = cheerio.load(body);
 
-			res.send(result);
-		}
-		else
-		{
-			res.send({x: "something went wrong"})
-		}
-	});
+      var result = {
+        title: parsedDocument("meta[name='twitter:text:main_song_title']").attr("content"),
+        artist: parsedDocument("meta[name='twitter:text:main_song_artist']").attr("content"),
+        image: parsedDocument("meta[name='twitter:image:song_thumbnail_img']").attr("content")
+      }
 
+      res.send(result);
+    }
+    else
+    {
+      res.send({x: "something went wrong"})
+    }
+  });
 });
 
 var port = Number(process.env.PORT || 5000);
