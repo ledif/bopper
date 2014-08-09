55beaca8de460c067eee31ba5c0288f5e253a418
Adam Fidel <refuse@gmail.com>
2014-08-09 15:23:53 -0500

    Adding support for artist and song-name based queries.

---
+++ bopper.js
@@ -0,0 +1,55 @@
+ var request = require('request'),
+     cheerio = require('cheerio');
+
+var getByURI = function(uri, cb) {
+  // if the url doesn't have http, add it
+  if (uri.lastIndexOf("http://", 0) !== 0)
+    uri = "http://" + uri;
+
+  request({uri : uri}, function(error, response, body) {
+    if (!error && response.statusCode === 200)
+    {
+      var parsedDocument = cheerio.load(body);
+
+      var result = {
+        title: parsedDocument("meta[name='twitter:text:main_song_title']").attr("content"),
+        artist: parsedDocument("meta[name='twitter:text:main_song_artist']").attr("content"),
+        image: parsedDocument("meta[name='twitter:image:song_thumbnail_img']").attr("content"),
+        uri: uri
+      }
+
+      cb(null, result)
+    }
+    // send empty result if document doesn't exist, or other http error
+    else
+    {
+      cb(error, {})
+    }
+  });
+}
+
+var boppify = function(str) {
+  str = str.toLowerCase();
+
+  // replace stuff with hyphens
+  str = str.replace(/ |\/|\\/g, "-")
+
+  // get rid of everything that's not a hyphen, period or alphanumeric
+  str = str.replace(/[^a-zA-Z0-9-.]/g, '')
+  return str
+}
+
+var uriFromArtistSong = function(artist, song) {
+  s = "http://bop.fm/s/" + boppify(artist) + "/" + boppify(song);
+  // console.log(s);
+  return s
+}
+
+var getByArtistSong = function(artist, song, cb) {
+  getByURI(uriFromArtistSong(artist, song), cb)
+}
+
+module.exports.getByURI = getByURI
+module.exports.getByArtistSong = getByArtistSong
+module.exports.boppify = boppify
+module.exports.uriFromArtistSong = uriFromArtistSong
---
+++ test/bopper.js
@@ -0,0 +1,42 @@
+var expect = require("chai").expect;
+var bopper = require("../bopper.js");
+ 
+describe("boppify", function() {
+	it("replace spaces with hyphens", function(){
+           var title = "too much"
+           var result = bopper.boppify(title)
+ 
+           expect(result).to.equal("too-much");
+    });
+
+    it("lowercase the damn thing", function(){
+           var title = "Too Much"
+           var result = bopper.boppify(title)
+ 
+           expect(result).to.equal("too-much");
+    });
+
+    it("remove extra symbols", function(){
+           var title = "7/4 (Shoreline)"
+           var result = bopper.boppify(title)
+ 
+           expect(result).to.equal("7-4-shoreline");
+    });
+
+    it("keep periods", function(){
+           var title = "I. crawl"
+           var result = bopper.boppify(title)
+ 
+           expect(result).to.equal("i.-crawl");
+    });
+});
+
+describe("url from song and artist", function() {
+	it("nothing special", function(){
+           var artist = "Drake"
+           var song = "Too Much"
+           var result = bopper.uriFromArtistSong(artist, song)
+ 
+           expect(result).to.equal("http://bop.fm/s/drake/too-much");
+    });
+});
